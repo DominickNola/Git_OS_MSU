7236a4be01f5f943b1787513ecb4bbc9acfc6d81
Dominick Licciardi <dominick.nola@gmail.com>
2019-06-06 21:31:12 -0600

    added sleep()

--- Assignment_1/counter.c
+++ Assignment_1/counter.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
     assert(printf("Parent PID: %d\n", parent_pid) != 0);
     for(int i = 1; i <= str_to_int; i++)
     {
+        usleep(250000); // sleep for 1/4 of a second.
         assert(printf("Process: %d %d\n", child_pid, i) != 0);
     }
 
