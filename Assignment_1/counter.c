e09aee871d9512512bc39cce08e47c91b8ac705c
Dominick Licciardi <dominick.nola@gmail.com>
2019-06-06 19:05:44 -0600

    Update counter.c

--- Assignment_1/counter.c
+++ Assignment_1/counter.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
 
+int main(int argc, char *argv[]) {
+
+    int child_pid = getpid();
+    int parent_pid = getppid();
+    char *end;
+
+    int str_to_int = strtol(argv[1], &end, 10);
+    assert(printf("Parent PID: %d\n", parent_pid) != 0);
+    for(int i = 1; i <= str_to_int; i++)
+    {
+        assert(printf("Process: %d %d\n", child_pid, i) != 0);
+    }
+
+    return str_to_int;
+}
