dd60639e05013d0012962af3c49c2f070dabb212
Dominick Licciardi <dominick.nola@gmail.com>
2019-06-11 00:17:13 -0600

    added edits

--- Assignment_1/main.c
+++ Assignment_1/main.c
@@ -6,34 +6,35 @@
 
 int main(int argc, char *argv[]) {
 
-    char string_num[10];
     char *terminal_input = argv[1];
+    char string_num[10];
+    int fork_child = fork();
+    int exit_status;
+
     if(terminal_input == NULL) {
         memcpy(string_num, "5", sizeof("5") + 1);
     } else {
         memcpy(string_num, argv[1], sizeof(terminal_input) + 1);
     }
 
-    int fork_child = fork();
-    int exit_status;
     if(fork_child  < 0) {
+
         perror("fork() error!\n\n");
         exit(-1);
     } else if(fork_child == 0) {
 
-        // child process block
+        // child process
         assert(printf("\nChild PID: %d\n", getpid()) >= 0);
         execl("./counter", "counter", string_num, (char*)NULL);
         perror("execl() error!\n\n");
         exit(1);
     } else {
 
-        // parent process block
-        waitpid(-1, &exit_status, 0);
-        // perror("waitpid() error!\n\n");
-        assert(printf("Process %d exited with status: %d\n\n", fork_child, WEXITSTATUS(exit_status)) >= 0);
+        // parent process
+        assert(waitpid(-1, &exit_status, 0) != 0);
+        assert(printf("Process %d exited with status: %d\n\n", fork_child,
+                      WEXITSTATUS(exit_status)) >= 0);
     }
 
     return 0;
 }
-
