1f18944b6823871cd44299901200181108132114
Dominick Licciardi <dominick.nola@gmail.com>
2019-06-20 22:26:47 -0600

    Update main.c

--- Assignment_2/main.c
+++ Assignment_2/main.c
@@ -6,60 +6,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void SIGUSR1_Handler();
-void SIGUSR2_Handler();
-void SIGHUP_Handler();
+void signal_handler();
 
 int main() {
 
-    signal(SIGUSR1, SIGUSR1_Handler);
-    signal(SIGUSR2, SIGUSR2_Handler);
-    signal(SIGHUP, SIGHUP_Handler);
-    printf("inside main.c\n");
-    int exitStatus;
-    // pid_t parentPID;
-    pid_t fork_child = fork();
+    int exit_status;
 
+    struct sigaction action;
+    action.sa_handler = signal_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = SA_RESTART;
+
+    assert(sigaction(SIGUSR1, &action, NULL) == 0);
+    assert(sigaction(SIGUSR2, &action, NULL) == 0);
+    assert(sigaction(SIGHUP, &action, NULL) == 0);
+
+    pid_t fork_child = fork();
     if( fork_child < 0) {
 
         perror("fork() error.\n");
         exit(-1);
     } else if(fork_child == 0) {
 
-        printf("inside child.\n");
-        exitStatus = execl("./child", "child", NULL);
+        exit_status = execl("./child", "child", NULL);
         perror("execl() error.\n");
-        exit(exitStatus);
+        exit(exit_status);
     } else {
 
-        assert(waitpid(fork_child, &exitStatus, 0) != -1);
-        printf("inside else, child done.\n");
+        assert(waitpid(fork_child, &exit_status, 0) != -1);
     }
 
     return 0;
-
 }
 
-void SIGUSR1_Handler(int signal) {
+void signal_handler(int signal) {
 
-    //sigaction();
-    printf("SIGUSR1_Handler:\n");
-    printf("User defined process 1.\n");
-    //then wait for the child
-    //waitpid();
-    //and then when signals arrive, the signal handler begins doing its thing.
+    if(signal == 1) {
 
-}
+        assert(write(STDOUT_FILENO, "Signal# 1, SIGHUP = SIGNAL HANGUP.\n", 35) != 0);
 
-void SIGUSR2_Handler(int signal) {
+    } else if(signal == 30) {
 
-    printf("SIGUSR2_Handler:\n");
-    printf("User defined process 2.\n");
+        assert(write(STDOUT_FILENO, "Signal# 30, SIGUSR1 = USER DEFINED 1.\n", 39) != 0);
 
-}
+    } else if(signal == 31) {
 
-void SIGHUP_Handler(int signal) {
+        assert(write(STDOUT_FILENO, "Signal# 31, SIGUSR2 = USER DEFINED 2.\n", 39) != 0);
 
-    printf("SIGHUP:\n");
-    printf("HANGUP or DEATH DETECTED!\n");
+    } else {
+
+        assert(write(STDOUT_FILENO, "Signal is invalid.\n", 19) != 0);
+    }
+
+     // maybe make 1 handler per signal
+     // Future dictionary/array of signal functions.
+//    char *dict[32] = {"invalid", "SIGHUP", " ", " ", " ", " ", " ",
+//                      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+//                      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+//                      " ", " ", " ", "SIGUSR1", "SIGUSR2"};
+//
+//    printf("Signal# %d, %s\n", signal, dict[signal]);
+
 }
